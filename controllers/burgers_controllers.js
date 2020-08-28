fd18b925ec0e5912da4bbc0c9de5157066c1a9a3
Anthony Bergen <anthonydbergen@gmail.com>
2020-08-28 11:02:32 -0400

    fixed connection to controller and model

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-var burger = require("../models/cat.js");
+var burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
   cat.all(function (data) {
