e00fca9dd03ee951e32c77087051eba33a9b365e
Anthony Bergen <anthonydbergen@gmail.com>
2020-08-28 11:22:51 -0400

    fixed mispelling hbsObject

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,7 +6,7 @@ var burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
   burger.all(function (data) {
-    var hbsObect = {
+    var hbsObject = {
       burgers: data,
     };
     console.log(hbsObject);
